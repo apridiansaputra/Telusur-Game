6e2522537a76e1f9c80be4cb5139d3538768d2d8
Afen Marbun <afenmarbun@gmail.com>
2024-05-19 11:00:58 +0700

    update animasi karakter

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ class GameObject:
     def __init__(self, position, scale):
         self.rect = pygame.Rect(position[0], position[1], scale[0], scale[1])
 
+
 class Character(GameObject):
     def __init__(self, position=(10, 50)):
         super().__init__(position, (135, 135))
         self.frame_index = 0
         self.frame_counter = 0
         self.frame_speed = 5
-        self.image_frames = [pygame.image.load(f"./assets/character/adventurer_walk{i+1}.png").convert_alpha() for i in range(2)]  # Load 4 frames
-        self.original_image = self.image_frames[self.frame_index]
+        self.image_frames_left = [pygame.image.load(f"./assets/character/adventurer_walk_left{i+1}.png").convert_alpha() for i in range(2)]
+        self.image_frames_right = [pygame.image.load(f"./assets/character/adventurer_walk_right{i+1}.png").convert_alpha() for i in range(2)]
+        self.image_frames_up = [pygame.image.load(f"./assets/character/adventurer_walk_up{i+1}.png").convert_alpha() for i in range(2)]
+        self.original_image = self.image_frames_right[self.frame_index]
         self.image = pygame.transform.scale(self.original_image, self.rect.size)
         self.has_key = False
         self.sound_manager = SoundManager()
+        self.last_direction = "right"
 
     def move(self, dx, dy, walls, screen_width, screen_height, camera_x, camera_y):
         old_x, old_y = self.rect.x, self.rect.y
@@ -27,10 +31,21 @@ class Character(GameObject):
 
         if dx != 0 or dy != 0:
             self.frame_counter += 1
+            if dx > 0 or dy > 0:
+                self.last_direction = "right"
+            elif dx < 0:
+                self.last_direction = "left"
+            elif dy < 0:
+                self.last_direction = "up"
             if self.frame_counter >= self.frame_speed:
                 self.frame_counter = 0
-                self.frame_index = (self.frame_index + 1) % len(self.image_frames)
-                self.original_image = self.image_frames[self.frame_index]
+                self.frame_index = (self.frame_index + 1) % len(self.image_frames_right)  # Assuming all sets of frames have the same length
+                if self.last_direction == "right":
+                    self.original_image = self.image_frames_right[self.frame_index]
+                elif self.last_direction == "left":
+                    self.original_image = self.image_frames_left[self.frame_index]
+                elif self.last_direction == "up":
+                    self.original_image = self.image_frames_up[self.frame_index]
                 self.image = pygame.transform.scale(self.original_image, self.rect.size)
 
         for wall in walls:
